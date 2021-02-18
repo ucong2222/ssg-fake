e692f7435bee5d9d3c21540cc4ccc5f122039954
ucong2222 <dbrudrjf21@gmail.com>
2021-02-18 17:06:10 +0900

    배포

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+function MobileTopBar__init() {
+  $(".mobile-top-bar__btn-toggle-side-bar").click(function () {
+    let $this = $(this);
 
+    if ($this.hasClass("active")) {
+      $this.removeClass("active");
+      $(".mobile-side-bar").removeClass("active");
+      $("html").removeClass("mobile-side-bar-actived");
+    } else {
+      $this.addClass("active");
+      $(".mobile-side-bar").addClass("active");
+      $("html").addClass("mobile-side-bar-actived");
+    }
+  });
+}
+
+MobileTopBar__init();
 function ArticleDetail__Body__init() {
 	if (toastui === undefined) {
 		return;
